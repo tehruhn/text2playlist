9138935187cd0814149e23e08ba80ac9dad120ca
Tarun <trolldemort9@gmail.com>
2021-07-30 12:18:39 +0530

    added longest paths as an option

--- text2playlist.py
+++ text2playlist.py
@@ -125,14 +125,13 @@ def parse_path(word_list, path):
     return phrases
 
 
-def generate_playlist_from_text(input_str, n=4, output=False):
+def generate_playlist_from_text(input_str, n=4, output=False, only_longest_path=True):
     """
     will check upto 4grams
     returns all paths as LIST OF DICTS
     where each dict is one path, and in a dict,
     where key is phrase
     and value is spotify uri
-    returns longest path currently
     """
     clean_words = clean_words_from_string(input_str)
     empty_graph = graph_from_clean_words(clean_words)
@@ -152,19 +151,30 @@ def generate_playlist_from_text(input_str, n=4, output=False):
     for path in all_paths_generator:
         word_paths.append(parse_path(clean_words, path))
 
-    # only return longest path
-    longest_path = max(word_paths, key = lambda i: len(i))
+    if only_longest_path :
+        # only return longest path
+        longest_path = max(word_paths, key = lambda i: len(i))
 
-    word_path_dict = {}
-    for word in longest_path:
-        word_path_dict[word] = uri_dict[word]
-    if output:
-        print("final longest word path :", longest_path)
-    return word_path_dict
+        word_path_dict = {}
+        for word in longest_path:
+            word_path_dict[word] = uri_dict[word]
+        if output:
+            print("final longest word path :", longest_path)
+        return word_path_dict
 
+    else :
+        word_path_dict_list = []
+        for word_path in word_paths:
+            word_path_dict = {}
+            for word in word_path:
+                word_path_dict[word] = uri_dict[word]
+            word_path_dict_list.append(word_path_dict)
+        if output:
+            print("all final word paths :", word_paths)
+        return word_path_dict_list
 
 
 if __name__ == "__main__":
-    test_str = "random shit"
+    test_str = "thanks for everything"
     songs_dict = generate_playlist_from_text(test_str, output=True)
-    print(songs_dict)
+    # print(songs_dict)
